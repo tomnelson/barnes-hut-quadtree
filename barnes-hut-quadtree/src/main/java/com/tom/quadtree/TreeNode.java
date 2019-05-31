780dabed14ffd8ce68ad527a0f48f7624bfdcb7a
tom <thomas.a.nelson@gmail.com>
2019-05-30 20:16:41 -0400

    first commit

---
+++ barnes-hut-quadtree/src/main/java/com/tom/quadtree/TreeNode.java
@@ -0,0 +1,12 @@
+package com.tom.quadtree;
+
+import java.awt.geom.Rectangle2D;
+import java.util.Collection;
+
+/** @author Tom Nelson */
+public interface TreeNode {
+
+  Rectangle2D getBounds();
+
+  Collection<? extends TreeNode> getChildren();
+}
